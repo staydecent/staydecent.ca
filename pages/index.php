696c4286685c6ac1d6aa8d255dd7e5a2b66238c7
Adrian Unger <dev@staydecent.ca>
2011-12-23 12:21:36 -0800

    temp page

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Staydecent</title>
+
+    <meta name="description" content="Staydecent is the web design and development studio of Adrian Unger.">
+    <meta name="author" content="Adrian Unger">
+</head>
+<body>
+
 <h1>Relaunching in the new year.</h1>
 
 <p>Staydecent is having a slight rebrand and facelift in the new year.</p>
 <p>I'm currently booked until February 1st, 2012. If you'd like to discuss a project, email me at <a href="mailto:hello&#64;staydecent.ca">hello&#64;staydecent.ca</a>.</p>
 
 <p><a href="http://twitter.com/staydecent">@staydecent</a></p>
+
+</body>
+</html>
