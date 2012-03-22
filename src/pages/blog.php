18de09c937d100fbcefb11665bb01f1cfcb61b0a
staydecent <dev@staydecent.ca>
2012-03-22 13:39:14 -0700

    /blog same content as /archives

--- src/pages/blog.php
+++ src/pages/blog.php
@@ -2,8 +2,9 @@
 
 <div role="main" id="main" class="container-fluid">
 
-    <h1>Archive</h1>
+    <h1>Archives</h1>
 
+    <ol class="styled">
     <?php
     $posts = $entries['articles'] + $entries['bits'];
     krsort($posts);
@@ -13,23 +14,15 @@
     {
         if (++$limit > 44) break;
         ?>
-        <article>
-            <header>
-                <h2><a class="black" href="<?php echo $entry['url']; ?>"><?php echo $entry['title'] ?></a></h2>  
-            </header>
-            
-            <p><?php echo $entry['excerpt'] ?></p>
-
-            <a class="btn-small" href="<?php echo $entry['url']; ?>" title="Permalink to <?php echo $entry['title'] ?>">Continue reading&hellip;</a>
-
-            <footer class="meta">
-                <i class="icon-calendar"></i> <a href="<?php echo $entry['url']; ?>">Published <?php echo date("F j, Y", $date) ?></a>
-                <i class="icon-align-left"></i> <?php echo str_word_count($entry['body']) ?> words
-            </footer>
-        </article>
+        <li>
+            <a href="<?php echo $entry['url']; ?>"><?php echo $entry['title'] ?></a>
+            &nbsp;&middot;
+            <span class="meta date"><?php echo date("M j, Y", $date) ?></span>
+        </li>
         <?php
     }
     ?>
+    </ol>
 
 </div>
 
