add4853f197c7a1749d3c4b87e5f8ef40f1f56f3
staydecent <dev@staydecent.ca>
2012-04-20 10:19:00 -0700

    slight style changes, new entry, new draft

--- src/assets/js/main-011.js
+++ src/assets/js/main-011.js
@@ -1,53 +1,16 @@
 /*! main-011.js: Staydecent.ca website enhancements. (c) Adrian Unger. Public Domain. */
 jQuery(document).ready(function($) {
-
-    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * Animated Scroll to Menu                                                *
-    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-    var menu_selector = window.location.hash;
-    if (menu_selector) {
-        $("html,body").animate({scrollTop:$(document).height()}, 800);
-    }
-
-    $('#menu-anchor').click(function(e){
-        $("html,body").animate({scrollTop:$(document).height()}, 800);
-        e.preventDefault();
-    });
     
 
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    * Trigger Scale Animations                                               *
+    * Image Captions                                                         *
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-    $(window).bind('scroll load', function(event) 
-    {
-        var fold = $(window).height() + $(window).scrollTop();
+    $('img[title]').each(function(i,e) {
+        console.log(i,e);
 
-        $('.scale').each(function(i) 
-        {
-            var e = $(this);
-
-            if (fold >= e.offset().top + 100) 
-            {
-                window.setTimeout(function() {
-                    e.addClass('animate');
-                }, i*100);
-            }
-        });
-
-        $('.invisible').each(function(i) 
-        {
-            var e = $(this);
-
-            if (fold >= e.offset().top + 100) 
-            {
-                window.setTimeout(function() {
-                    e.css({
-                        visibility:'visible'
-                    });
-                }, i*300);
-            }
-        });
+        var title = $(e).attr('title');
+        $(e).after('<div class="caption">'+title+'</div>');
+        $(e).next('.caption').fadeIn(500);
     });
 
 });
