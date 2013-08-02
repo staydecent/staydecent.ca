8a7c6a6f397c05c8268236bf34733a7ab7decb27
Adrian Unger <dev@staydecent.ca>
2013-08-01 17:10:32 -0700

    Layout slights. New tracking.

--- src/layout/footer.php
+++ src/layout/footer.php
@@ -4,7 +4,11 @@
     <article class="container-fluid">
         <p class="centre"><img src="<?php echo SITE_URL ?>favicon.ico" alt="Trees are pretty great."></p>
         <p>I&rsquo;m Adrian Unger. I build <a href="http://ecquire.com" target="_blank">Ecquire</a> and advocate for biodiversity. Generally, I spend more time among trees than people.</p>
+        <?php if ($title === 'About'): ?>
+        <p><small><a href="<?php echo SITE_URL ?>archives" title="view all posts">Archives</a></small></p>
+        <?php else: ?>
         <p><small><a href="<?php echo SITE_URL ?>about" title="about adrian unger">Say hello</a></small></p>
+        <?php endif; ?>
     </article>
 
 </footer>
@@ -14,6 +18,23 @@
 <?php echo (isset($scripts)) ? $scripts : ''; ?>
 <script src="<?php echo SITE_URL ?>assets/js/plugins.min.js"></script>
 <script src="<?php echo SITE_URL ?>assets/js/main-012.js"></script>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.staydecent.ca"]);
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.staydecent.ca/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "2"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
 
 </body>
 </html>
--- src/pages/about.php
+++ src/pages/about.php
@@ -8,8 +8,8 @@
             <p class="meta centre">You can reach me via <a href="mailto:hello&#64;staydecent.ca">Email</a> or <a href="http://twitter.com/staydecent" target="_blank">Twitter</a>.</p>
         </header>
 
-        <div class="container-fluid">            
-            <p class="big">I&rsquo;m a software developer at <a href="http://ecquire.com" target="_blank">Ecquire</a>. Previously, I ran a freelance web development practice under the alias <em>Staydecent</em>. Currently, I&rsquo;m hanging out in <a href="http://vivaoaxaca2013.wordpress.com" target="_blank">Mexico</a>.</p>
+        <div class="container-fluid">
+            <p class="big">I&rsquo;m a software developer at <a href="http://ecquire.com" target="_blank">Ecquire</a>. Previously, I ran a freelance web development practice under the alias <em>Staydecent</em>.</p>
 
             <p>I disagree with institutionalized education, offices and economics. That&rsquo;s why I dropped out of college, work remotely and support small-time makers&mdash;and growers! Moreover, I&rsquo;d say I'm an advocate for biodiversity.</p>
 
@@ -19,15 +19,15 @@
 
             <p>You can <a href="http://github.com/staydecent/staydecent.ca" target="_blank">view the source</a> on Github.</p>
         </div>
-    </article>  
+    </article>
 
 </div>
 
-<?php 
+<?php
 $next = array();
 
 $next['url'] = 'contact';
 $next['text'] = 'Say hello!';
-?>          
+?>
 
 <?php include $config['dirs']['layout'].'/footer.php' ?>
