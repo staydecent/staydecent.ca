86a30f949f7bd7fc884ab068a521a4a5ad6c566c
Adrian Unger <dev@staydecent.ca>
2014-10-14 10:35:21 -0300

    Add new/missing content

--- src/layout/header.php
+++ src/layout/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Adrian Unger">
-    <meta name="description" content="Adrian Unger is a web developer living in the beautiful west coast of BC. He writes about design, nature, technology, etc.">
+    <meta name="description" content="Adrian Unger is the CTO of Ecquire. He lives in Canada, but prefers México. He also loves farming/gardening.">
 
     <title><?php 
         if (stristr($title, 'adrian'))
@@ -14,10 +14,6 @@
             echo $title.' &middot; Adrian Unger';
     ?></title>
 
-    <link rel="stylesheet" href="<?php echo SITE_URL ?>assets/css/bootstrap.css">    
-    <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <script src="<?php echo SITE_URL ?>assets/js/respond.min.js"></script>
+    <link rel="stylesheet" href="<?php echo SITE_URL ?>assets/css/bootstrap.css">
 </head>
 <body>
