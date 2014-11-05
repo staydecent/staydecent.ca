de968ec4fa8ac606a3f321ba274c77c71b3cec56
Adrian Unger <dev@staydecent.ca>
2014-11-05 15:42:06 -0400

    Update stylez

--- src/layout/header.php
+++ src/layout/header.php
@@ -14,11 +14,10 @@
       echo $title.' ~ staydecent.ca';
   ?></title>
   
-  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
   <style type="text/css">
     body { 
-      font-family: 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
-      font-size: 21px;
+      font-family: 'Monaco', 'Consolas', 'Menlo', monospace;
+      font-size: 16px;
       line-height: 1.6;
       color: black;
       background: #fff;
@@ -26,37 +25,42 @@
     }
     a {
       color: black;
-      border-bottom: 1px solid black;
-      text-decoration: none;
+      border-radius: 4px;
+      transition-property: color, background-color, box-shadow, opacity;
+      transition-duration: 0.2s;
+      transition-timing-function: ease-out;
     }
     a:hover {
-      color: white;
-      border-color: #E47964;
-      background-color: #FFDEC9;
+      text-decoration: none;
+      color: #FFDEC9;
+      background-color: #4433AA;
+      box-shadow: 0 0 7px #4433AA;
     }
     h1,h2,h3,h4,h5 {
       font-weight: 400;
     }
+    nav > ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      font-size: 75%;
+    }
+    nav a {
+      text-decoration: none;
+    }
     #main,
     .container {
-      margin-top: 4em;
       max-width: 1080px;
-      margin: 0 auto;
+      margin: 5% auto;
       clear: both;
     }
     #main p {
+      font-size: 18px;
+      font-family: 'georgia', serif;
       max-width: 640px;
     }
-    #forest {
-      margin: 20px 0;
-    }
-    #forest:after {
-      display: block;
-      content: ' ';
-      height: 150px;
-    }
     #main blockquote > p {
-      font: italic 21px/1.7 'Georgia', serif;
+      font-style: italic;
     }
     header h1, header h2 {
       margin: 0;
@@ -78,56 +82,14 @@
       margin: 0;
       padding-left: 2em;
     }
-    .plot {
-      width: 16px;
-      height: 16px;
-      float: left;
-      overflow: hidden;
-    }
-    .tree {
-      width: 16px;
-      height: 16px;
-      background-image: url(<?php echo SITE_URL ?>favicon.ico);
-      background-repeat: no-repeat;
-      background-position: 0px 0px;
-      opacity: 0;
-      transition: opacity .2s ease-out;
-    }
-    .grow {
-      opacity: 1;
-      animation: grow-tree .7s ease .2s;
-      -webkit-animation: grow-tree .7s ease .2s;
-    }
-    @keyframes grow-tree {
-       from { transform: translateY(16px); }
-       to   { transform: translateY(0px); }
-    }
-    @-webkit-keyframes grow-tree {
-       from { -webkit-transform: translateY(16px); }
-       to   { -webkit-transform: translateY(0px); }
-    }
   </style>
 </head>
 <body>
 
-<div class="container">
-<small>
-  <a href="/" id="site-title">Hello</a>
-  /
-  <a href="<?php echo SITE_URL ?>archives" title="view all posts">Archives</a>
-  /
-  <a href="<?php echo SITE_URL ?>about" title="about adrian unger">Information</a>
-</small>
-</div>
-
-<div id="forest">
-<?php 
-$i = 730;
-while ($i > 0):
-?>
-<div class="plot"><?php if (rand(0, 1)): ?><div class="tree"></div><?php endif; ?></div>
-<?php
-$i--;
-endwhile;
-?>
-</div>
+<nav class="container">
+<ul>
+  <li><a href="/" id="site-title">Home</a></li>
+  <li><a href="<?php echo SITE_URL ?>archives" title="view all posts">Archives</a></li>
+  <li><a href="<?php echo SITE_URL ?>about" title="about adrian unger">Information</a></li>
+</ul>
+</nav>
