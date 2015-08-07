7ba3aa584d9f1cc7302ff2f7f7d7a90daa39ec40
Adrian Unger <dev@staydecent.ca>
2015-08-07 13:02:50 -0700

    updated index to reference surge

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <a href="https://instagram.com/p/4zycNEnOFI/?taken-by=staydecent" target="_blank"><img src="http://i.imgur.com/3nhbej3.jpg" alt="i like trees" /></a>
 
-            <p>Hey, I'm Adrian. I enjoy food, nature, am probably a neophile, and make web and mobile apps <a href="http://inputlogic.ca" title="Input Logic Inc." target="_blank">for a living</a>. This site is powered by <a href="http://www.hydroquebec.com/about-hydro-quebec/our-energy/" target="_blank" title="Hydro-Québec">99% renewables</a> and is <a href="http://github.com/staydecent/staydecent.ca" target="_blank">open sourced</a>.</p>
+            <p>Hey, I'm Adrian. I enjoy food, nature, am probably a neophile, and make web and mobile apps <a href="http://inputlogic.ca" title="Input Logic Inc." target="_blank">for a living</a>. This site is powered by <a href="http://surge.sh" target="_blank" title="surge">surge.sh</a> and is <a href="http://github.com/staydecent/staydecent.ca" target="_blank">open sourced</a>.</p>
 
             <p class="centre">You can reach me via <a href="mailto:hello&#64;staydecent.ca">Email</a> or <a href="http://twitter.com/staydecent" target="_blank">Twitter</a>.</p>
         </div>
