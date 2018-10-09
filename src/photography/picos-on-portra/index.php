d22d47925bb43bd27b098d16a451beff6e74248b
Adrian Unger <dev@staydecent.ca>
2018-10-08 22:21:14 -0700

    WIP add Picos on Portra gallery.

---
+++ src/photography/picos-on-portra/index.php
@@ -0,0 +1,107 @@
+<?php 
+  $title = 'Picos on Portra'
+?>
+
+<?php include $config['dirs']['layout'].'/header.php' ?>
+
+<?php 
+  $base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
+?>
+
+<div id="picos" class="book">
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_2.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid">
+    <div class="photo">
+      <div class="text">
+        <div class="title">
+          <h1>Picos on Portra</h1>
+          <p class="big">In September of 2018 I left the North American continent for the first time, to spend three weeks in Spain.</p>
+          <p>Part of the trip involved hiking in the Picos De Europa national park in northern Spain. These are some selected photos from a roll of Kodak Portra 160. All shot with a Yashica FX-D and Yashica ML 50mm/2.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_6.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_10.jpg" alt="Picos De Europa Photo by Adrian Unger">      
+    </div>
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_5.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_11.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_1.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_2.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_9.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_4.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_13.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_12.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid spread">
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_3.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+  <div class="grid">
+    <div class="photo">
+      <div class="text">
+        <div class="title">
+          <h2>Thanks for stopping by!</h2>
+          <h2>For prints or inquiries, <br /><a href="mailto:photo@staydecent.ca">send me an email</a>.</h2>
+          <h2><a href="<?php echo SITE_URL ?>photography">View more galleries.</a></h2>
+        </div>
+      </div>
+    </div>
+    <div class="photo">
+      <img src="<?php echo $base_url ?>/images/picos_7.jpg" alt="Picos De Europa Photo by Adrian Unger">
+    </div>
+  </div>
+
+</div>
+
+<?php include $config['dirs']['layout'].'/footer.php' ?>
