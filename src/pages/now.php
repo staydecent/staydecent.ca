666e9526c8a6baf2fd10353b5302a43bd6609d6b
Adrian Unger <dev@staydecent.ca>
2019-02-20 20:11:25 -0800

    Update now.

--- src/pages/now.php
+++ src/pages/now.php
@@ -10,9 +10,8 @@
       <ul>
         <li>Building <span title="Progressive Web Apps">PWAs</span> and React Native apps with <a href="http://inputlogic.ca" target="_blank">Input Logic</a></li>
         <li>Shooting <a href="/blog/expired-kodak-bwc" target="_blank">more film</a> than I used to</li>
-        <li><del><a href="https://run-log.com/profiles/profile/staydecent/" target="_blank">Running</a></del> <ins title="Commuting by bike has replaced running">Road and Mountain Biking</ins> as much as possible</li>
-        <li>Getting back into skateboarding after 8-10 years! </li>
-        <li>Trying to design more in my spare time</li>
+        <li><del><a href="https://run-log.com/profiles/profile/staydecent/" target="_blank">Running</a></del> <del>Road and Mountain Biking</del> <ins title="I just got back into Skateboarding after a decade">Skateboarding</ins> as much as possible</li>
+        <li>Trying to get my lazy ass to make some photo-zines!</li>
       </ul>
     </div>
 
@@ -20,7 +19,7 @@
 
     <div><p>This page was inspired by&nbsp;<a href="https://sivers.org/now">Derek Sivers</a>.</p></div>
 
-    <div><p><em>Last updated:</em> 15th October, 2018</p></div>
+    <div><p><em>Last updated:</em> 20th February, 2019</p></div>
   </div>
 
 <?php include $config['dirs']['layout'].'/footer.php' ?>
