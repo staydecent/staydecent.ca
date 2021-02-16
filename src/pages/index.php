89c9e404f175e25c6be19938dd770eee74d3c9ab
Adrian Unger <dev@staydecent.ca>
2021-02-15 16:05:17 -0800

    Better image loading on homepage.

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,55 +1,55 @@
 <?php $page_id = 'photography'; ?>
 <?php include $config['dirs']['layout'].'/header.php' ?>
-  <?php include $config['dirs']['layout'].'/nav.php' ?>
+<?php include $config['dirs']['layout'].'/nav.php' ?>
 
 <div class=container>
   <div class=content>
 
     <div>
       <a href="<?php echo SITE_URL ?>photography/picos-on-portra" data-color='F1E0D0'>
-        <img src='<?php echo SITE_URL ?>photography/picos-on-portra/images/picos_2.jpg' />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/picos-on-portra/images/picos_2.jpg' />
         <h5>Picos On Portra</h5>
       </a>
     </div>
 
     <div>
       <a href="<?php echo SITE_URL ?>photography/paris-noir" data-color='fff'>
-        <img src='<?php echo SITE_URL ?>photography/paris-noir/images/ParisNoir_7.jpg' />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/paris-noir/images/ParisNoir_7.jpg' />
         <h5>Paris Noir</h5>
       </a>
     </div>
 
     <div class=center>
       <a href="<?php echo SITE_URL ?>photography/paris-couleur" data-color='f3e7bd'>
-        <img src='<?php echo SITE_URL ?>photography/paris-couleur/images/ParisCouleur_8.jpg' />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/paris-couleur/images/ParisCouleur_8.jpg' />
         <h5>Paris Couleur</h5>
       </a>
     </div>
 
     <div>
       <a href="<?php echo SITE_URL ?>photography/waves" data-color='f9f3e1'>
-        <img src='<?php echo SITE_URL ?>photography/waves/images/waves_4.jpg' />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/waves/images/waves_4.jpg' />
         <h5>Waves</h5>
       </a>
     </div>
 
     <div class=right>
       <a href="<?php echo SITE_URL ?>photography/black-and-white" data-color='666'>
-        <img src='<?php echo SITE_URL ?>photography/black-and-white/images/bnw_1.jpg' alt="Black and White" />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/black-and-white/images/bnw_1.jpg' alt="Black and White" />
         <h5>Black &amp; White</h5>
       </a>
     </div>
 
     <div class='center extra-padding'>
       <a href="<?php echo SITE_URL ?>photography/maui" data-color='FBCBC1'>
-        <img src='<?php echo SITE_URL ?>photography/maui/images/maui_5.jpg' />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/maui/images/maui_5.jpg' />
         <h5>Maui</h5>
       </a>
     </div>
 
     <div class=col-2>
       <a href="<?php echo SITE_URL ?>photography/shinrinyoku" data-color='433C06'>
-        <img src='<?php echo SITE_URL ?>photography/shinrinyoku/images/shinrinyoku_1.jpg' />
+        <img loading='lazy' src='<?php echo SITE_URL ?>photography/shinrinyoku/images/shinrinyoku_1.jpg' />
         <h5>森林浴</h5>
       </a>
     </div>
