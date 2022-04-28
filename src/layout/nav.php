3665740b3d804cc0a1ab1712473a3c3a078bd6c3
Adrian Unger <dev@staydecent.ca>
2022-04-28 12:22:15 -0700

    Update nav.php

--- src/layout/nav.php
+++ src/layout/nav.php
@@ -1,9 +1,9 @@
+<?php /* ?>
 <nav>
   <a href="/" <?php echo current_class('adrian') ?><?php echo current_class('photography') ?> data-color='433C06'>Photography</a>
   <a href="<?php echo SITE_URL ?>code" <?php echo current_class('code') ?> data-color='ddd'>Software</a>
   <a href="<?php echo SITE_URL ?>blog" <?php echo current_class('blog') ?> data-color='B6CAC0'>Etc.</a>
 </nav>
-<?php /* ?>
 <nav>
   <a href="<?php echo SITE_URL ?>about" <?php echo current_class('about') ?> data-color='433C06'>About</a>
   <a href="<?php echo SITE_URL ?>now" <?php echo current_class('now') ?> data-color='433C06'>Now</a>
