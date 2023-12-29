22f02c8c277917dbe3e6ca41e85da05c813f2ecf
Adrian Unger <dev@staydecent.ca>
2023-12-29 15:34:55 -0800

    WIP.

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,71 +1,23 @@
-<?php $page_id = 'photography'; ?>
+<?php $page_id = 'index'; ?>
 <?php include $config['dirs']['layout'].'/header.php' ?>
 <?php include $config['dirs']['layout'].'/nav.php' ?>
 
-<div class=scroll>
-  <div class=grid>
-    <a class=story href="<?php echo SITE_URL ?>photography/sebastion-beach" data-color='f3f8ff'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/sebastion-beach/images/sebastion_10_dithered.jpg' />
-      <h5>Sebastion Beach</h5>
-    </a>
+<div class="viewport vp1">
+  <h1 class=vp1-title>Hi</h1>
+  <div class="vp1-content container">
+    <div class="grid-6">
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>
+        <p>This is my [Digital Garden](/what-is-a-digital-garden) where I aim to share my _unpolished_ perspective. Unpolished really just means that nothing is "done" on this website. Everything published is ephemeral (in spirit, I don't plan to delete much of anything), with the aim that my perspective changes and grows. The difference between a digital garden and a personal blog may seem subtle. Yet, for me, it's the key detail that frees myself to actually publish and update this website again—you may notice the [archive]() is quite limited and filled with rather infrequent posts.</p>
+      </div>
+      <div>&nbsp;</div>
+      <div>
+        <p>Personal blogs used to share the same _unfinished_ and rough draft vibe as a Digital Garden. With the onset of Twitter, unpolished ideas moved over there. And, for me, my perspective of personal blogs become more rigid: they were where the polished, edited ideas went after exploring them on Twitter. Since [leaving social media]() I haven't shared any photos or words on the internet. It's been so long, that I've even developed a limiting belief of "who am I to publish something?" I have to remind myself of the immense value and joy I get from others who share personal perspectives on the internet. I don't aim to be an authority on anything. Yet, perhaps, someone will still find joy—or even inspiration—by exploring this garden.</p>
 
-    <a class=story href="<?php echo SITE_URL ?>photography/july-town" data-color='D0DBD7'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/july-town/images/town_14_dithered.jpg' />
-      <h5>July Town</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/picos-on-portra" data-color='F1E0D0'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/picos-on-portra/images/picos_2_dithered.jpg' />
-      <h5>Picos On Portra</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/paris-noir" data-color='fff'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/paris-noir/images/parisnoir_7_dithered.jpg' />
-      <h5>Paris Noir</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/paris-couleur" data-color='f3e7bd'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/paris-couleur/images/pariscouleur_8_dithered.jpg' />
-      <h5>Paris Couleur</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/waves" data-color='f9f3e1'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/waves/images/waves_4_dithered.jpg' />
-      <h5>Waves</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/black-and-white" data-color='666'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/black-and-white/images/bnw_1_dithered.jpg' alt="Black and White" />
-      <h5>Black &amp; White</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/maui" data-color='FBCBC1'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/maui/images/maui_5_dithered.jpg' />
-      <h5>Maui</h5>
-    </a>
-
-    <a class=story href="<?php echo SITE_URL ?>photography/shinrinyoku" data-color='433C06'>
-      <img loading='lazy' src='<?php echo SITE_URL ?>photography/shinrinyoku/images/shinrinyoku_1_dithered.jpg' />
-      <h5>森林浴</h5>
-    </a>
-  </div>
-</div>
-
-<div class="container">
-  <div class="grid-50">
-    <h1>Welcome to my Digital Garden</h1>
-    <div>&nbsp;</div>
-  </div>
-  <div class="grid-3">
-    <div>&nbsp;</div>
-    <div>
-      <p>This is my [Digital Garden](/what-is-a-digital-garden) where I aim to share my _unpolished_ perspective. Unpolished really just means that nothing is "done" on this website. Everything published is ephemeral (in spirit, I don't plan to delete much of anything), with the aim that my perspective changes and grows. The difference between a digital garden and a personal blog may seem subtle. Yet, for me, it's the key detail that frees myself to actually publish and update this website again—you may notice the [archive]() is quite limited and filled with rather infrequent posts.</p>
-
-      <p>Personal blogs used to share the same _unfinished_ and rough draft vibe as a Digital Garden. With the onset of Twitter, unpolished ideas moved over there. And, for me, my perspective of personal blogs become more rigid: they were where the polished, edited ideas went after exploring them on Twitter. Since [leaving social media]() I haven't shared any photos or words on the internet. It's been so long, that I've even developed a limiting belief of "who am I to publish something?" I have to remind myself of the immense value and joy I get from others who share personal perspectives on the internet. I don't aim to be an authority on anything. Yet, perhaps, someone will still find joy—or even inspiration—by exploring this garden.</p>
-
-      <p>So, that's the context around this website. It's still in it's early stages of becoming a [Digital Garden](), but the intention is here. There are no analytics or tracking. I have no idea if anyone still finds this site. In case anyone does, feel free to [reach out to me here]().</p>
+        <p>So, that's the context around this website. It's still in it's early stages of becoming a [Digital Garden](), but the intention is here. There are no analytics or tracking. I have no idea if anyone still finds this site. In case anyone does, feel free to [reach out to me here]().</p>
+      </div>
     </div>
-    <div>&nbsp;</div>
   </div>
 </div>
 
