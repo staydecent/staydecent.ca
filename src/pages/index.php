f584dcf08935372717a2fe5c220b64a43a57dd53
Adrian Unger <dev@staydecent.ca>
2023-12-30 09:39:44 -0800

    WIP homepage.

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,9 +6,40 @@
   <h1 class=vp1-title>Welcome</h1>
   <div class="vp1-content container">
     <div class="grid-3">
+      <p class="big">Hi, my name's Adrian.</p>
+      <p class=big>This is my <a class=black href="">digital garden</a> where I share my perspective through <a class=black href="">words</a> and <a class=black href="">photos</a>. Most recently, I <a class=black href="">worked</a> as a Product Developer. But, right <a class=black href="">now</a>, I'm taking a sabbatical.</p>
+    </div>
+  </div>
+</div>
+
+<div class="viewport vp1" style="background: url('<?php echo SITE_URL ?>photography/sebastion-beach/images/sebastion_10_dithered.jpg') center center no-repeat;">
+  <h1 class=vp1-title style="font-weight: 600; color: #7c8ea8ab">Sebastion Beach</h1>
+  <div class="vp1-content container">
+    <div class="grid-3">
+      <p class=big>Winter</p>
+      <p class=big>2022</p>
+    </div>
+  </div>
+</div>
+
+<div class="viewport vp1" style="background: url('<?php echo SITE_URL ?>photography/july-town/images/town_14_dithered.jpg') center center no-repeat, linear-gradient(to top, #D0DBD7aa, #D0DBD7aa); justify-content: flex-end;">
+  <div class="vp1-content container">
+    <div class="grid-3">
       <div>&nbsp;</div>
       <div>
-        <p class=bigger>This is my <a class=grey href="">digital garden</a> where I share my perspective through <a class=grey href="">words</a> and <a class=grey href="">photos</a>. Most recently, I <a class=grey href="">worked</a> as a Product Developer. But, right <a class=grey href="">now</a>, I'm taking a sabbatical.</p>
+        <h1 class=vp2-title style="color: #C74938;">A Small Canadian Town In July</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="viewport vp1" style="background: url('<?php echo SITE_URL ?>photography/picos-on-portra/images/picos_11_dithered.jpg') left bottom no-repeat; justify-content: flex-start;">
+  <div class="vp1-content container">
+    <div class="grid-3">
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>
+        <h1 class=vp1-title style="font-size: 8vw; line-height: 1; color: #000;">Picos on Portra</h1>
       </div>
     </div>
   </div>
