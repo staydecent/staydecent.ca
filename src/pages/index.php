5c0af0185b5ea9d84f0cbd0acb238d22b1013bf6
Adrian Unger <dev@staydecent.ca>
2023-12-29 21:07:41 -0800

    Finish home intro section.

--- src/pages/index.php
+++ src/pages/index.php
@@ -3,19 +3,12 @@
 <?php include $config['dirs']['layout'].'/nav.php' ?>
 
 <div class="viewport vp1">
-  <h1 class=vp1-title>Hi</h1>
+  <h1 class=vp1-title>Welcome</h1>
   <div class="vp1-content container">
-    <div class="grid-6">
-      <div>&nbsp;</div>
-      <div>&nbsp;</div>
-      <div>
-        <p>This is my [Digital Garden](/what-is-a-digital-garden) where I aim to share my _unpolished_ perspective. Unpolished really just means that nothing is "done" on this website. Everything published is ephemeral (in spirit, I don't plan to delete much of anything), with the aim that my perspective changes and grows. The difference between a digital garden and a personal blog may seem subtle. Yet, for me, it's the key detail that frees myself to actually publish and update this website again—you may notice the [archive]() is quite limited and filled with rather infrequent posts.</p>
-      </div>
+    <div class="grid-3">
       <div>&nbsp;</div>
       <div>
-        <p>Personal blogs used to share the same _unfinished_ and rough draft vibe as a Digital Garden. With the onset of Twitter, unpolished ideas moved over there. And, for me, my perspective of personal blogs become more rigid: they were where the polished, edited ideas went after exploring them on Twitter. Since [leaving social media]() I haven't shared any photos or words on the internet. It's been so long, that I've even developed a limiting belief of "who am I to publish something?" I have to remind myself of the immense value and joy I get from others who share personal perspectives on the internet. I don't aim to be an authority on anything. Yet, perhaps, someone will still find joy—or even inspiration—by exploring this garden.</p>
-
-        <p>So, that's the context around this website. It's still in it's early stages of becoming a [Digital Garden](), but the intention is here. There are no analytics or tracking. I have no idea if anyone still finds this site. In case anyone does, feel free to [reach out to me here]().</p>
+        <p class=bigger>This is my <a class=grey href="">digital garden</a> where I share my perspective through <a class=grey href="">words</a> and <a class=grey href="">photos</a>. Most recently, I <a class=grey href="">worked</a> as a Product Developer. But, right <a class=grey href="">now</a>, I'm taking a sabbatical.</p>
       </div>
     </div>
   </div>
