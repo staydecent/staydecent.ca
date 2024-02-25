d0bfb73d4b81bcb0a953963c32f1fc3c7a7e2b23
Adrian Unger <dev@staydecent.ca>
2024-02-25 14:20:15 -0800

    update sebastion.

--- src/photography/sebastion-beach/index.php
+++ src/photography/sebastion-beach/index.php
@@ -1,13 +1,14 @@
 <?php 
-  $title = 'Sebastion Beach';
-  $page_id = 'sebastion';
+$title = 'Sebastion Beach';
+$page_id = 'sebastion';
+$body_class = 'photography';
 ?>
 
 <?php include $config['dirs']['layout'].'/header.php' ?>
 <?php include $config['dirs']['layout'].'/nav.php' ?>
 
 <?php 
-  $base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
+$base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
 ?>
 
 <main>
@@ -15,9 +16,75 @@
   <section id=sebastion>
     <div class=f-center>
       <h1 class=break>Sebastion Beach</h1>
-    </div>
-    <figure>
       <p class=bigger>Typical winter days captured at Sebastion Beach.</p>
+    </div>
+    <img src="<?php echo $base_url ?>/images/sebastion_10.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_15.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+      <img src="<?php echo $base_url ?>/images/sebastion_6.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+      <img src="<?php echo $base_url ?>/images/sebastion_4.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_5.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_3.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_7.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_13.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_9.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_11.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_12.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <div>&nbsp;</div>
+    <img src="<?php echo $base_url ?>/images/sebastion_1.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <figure class="contain">
+      <img src="<?php echo $base_url ?>/images/sebastion_2.jpg" alt="Sebastion Beach Photo by Adrian Unger">      
+    </figure>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/sebastion_16.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <div>&nbsp;</div>
+    <img src="<?php echo $base_url ?>/images/sebastion_8.jpg" alt="Sebastion Beach Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <figure class="contain">
+      <img src="<?php echo $base_url ?>/images/sebastion_17.jpg" alt="Sebastion Beach Photo by Adrian Unger">      
     </figure>
   </section>
 
@@ -26,76 +93,3 @@
 $in_viewport = true;
 include $config['dirs']['layout'].'/footer.php'
 ?>
-
-
-<?php /* ?>
-<div class=container>
-  <div class="content spaced">
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/sebastion_10.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_15.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
-        <img src="<?php echo $base_url ?>/images/sebastion_6.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-        <img src="<?php echo $base_url ?>/images/sebastion_4.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_5.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_3.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_7.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_13.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_9.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_11.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div>
-      <img src="<?php echo $base_url ?>/images/sebastion_12.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/sebastion_1.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_2.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/sebastion_16.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/sebastion_8.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
-      <img src="<?php echo $base_url ?>/images/sebastion_17.jpg" alt="Sebastion Beach Photo by Adrian Unger">
-    </div>
-
-  </div>
-</div>
-<?php */ ?>
