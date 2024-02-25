5f05d5a694a82636788cde776ac17748e07be889
Adrian Unger <dev@staydecent.ca>
2024-02-25 13:24:33 -0800

    update noir.

--- src/photography/paris-noir/index.php
+++ src/photography/paris-noir/index.php
@@ -1,86 +1,94 @@
 <?php 
-  $title = 'Paris Noir';
-  $page_id = 'noir';
+$title = 'Paris Noir';
+$page_id = 'noir';
+$body_class = 'photography';
 ?>
 
 <?php include $config['dirs']['layout'].'/header.php' ?>
 <?php include $config['dirs']['layout'].'/nav.php' ?>
 
 <?php 
-  $base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
+$base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
 ?>
 
-<div class='aside wide'>
-  <div class="title">
-    <h1>Paris Noir</h1>
-  </div>
+<main>
 
-  <div class="body">
-    <p class="bigger">In 2018, I visited Paris for the first time. Really, it was my frist time off the North American continent!</p>
-    <p class=big>After spending three weeks in Spain, my partner and I wandered around Paris for 4 days. These are some selected photos from a roll of Ilford XP2. All shot with a Yashica FX-D and Yashica ML 50mm/2.</p>
-  </div>
-</div>
+  <section>
+    <div class=f-center>
+      <h1 class=break>Paris Noir</h1>
+      <p class="display">In 2018, I visited Paris for the first time. Really, it was my frist time off the North American continent!</p>
+      <p >After spending three weeks in Spain, my partner and I wandered around Paris for 4 days. These are some selected photos from a roll of Ilford XP2. All shot with a Yashica FX-D and Yashica ML 50mm/2.</p>
+    </div>
+    <img src="<?php echo $base_url ?>/images/parisnoir_7.jpg" alt="Paris Photo by Adrian Unger">
+  </section>
 
-<div class=container>
-  <div class="content spaced">
 
-    <div>
+  <section>
+    <article class=f-center>
       <img src="<?php echo $base_url ?>/images/parisnoir_8.jpg" alt="Paris Photo by Adrian Unger">
+    </article>
+    <div class='f-end'>
+      <article class=f-center>
+        <img src="<?php echo $base_url ?>/images/parisnoir_11.jpg" alt="Paris Photo by Adrian Unger">
+      </article>
     </div>
+  </section>
 
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/parisnoir_7.jpg" alt="Paris Photo by Adrian Unger">
+  <section class=right>
+    <img src="<?php echo $base_url ?>/images/parisnoir_5.jpg" alt="Paris Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/parisnoir_13.jpg" alt="Paris Photo by Adrian Unger">
+    <img src="<?php echo $base_url ?>/images/parisnoir_1.jpg" alt="Paris Photo by Adrian Unger">
+  </section>
+
+
+  <section>
+    <div>&nbsp;</div>
+    <img src="<?php echo $base_url ?>/images/parisnoir_2.jpg" alt="Paris Photo by Adrian Unger">
+  </section>
+
+  <section class=right>
+    <figure class="contain">
+    <img src="<?php echo $base_url ?>/images/parisnoir_9.jpg" alt="Paris Photo by Adrian Unger">
+    </figure>
+  </section>
+
+  <section class=>
+    <img src="<?php echo $base_url ?>/images/parisnoir_6.jpg" alt="Paris Photo by Adrian Unger">
+  </section>
+
+  <section class=center>
+    <div class="f-end">
+      <img src="<?php echo $base_url ?>/images/parisnoir_14.jpg" alt="Paris Photo by Adrian Unger"> 
     </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/parisnoir_5.jpg" alt="Paris Photo by Adrian Unger">      
-    </div>
-
-    <div>
-      <img src="<?php echo $base_url ?>/images/parisnoir_13.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
-      <img src="<?php echo $base_url ?>/images/parisnoir_11.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/parisnoir_1.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
-
-    <div>
-      <img src="<?php echo $base_url ?>/images/parisnoir_2.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/parisnoir_9.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
-
-    <div class=>
-      <img src="<?php echo $base_url ?>/images/parisnoir_6.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
+    <article class=f-center>
       <img src="<?php echo $base_url ?>/images/parisnoir_10.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
+    </article>
+  </section>
 
-    <div>
-      <img src="<?php echo $base_url ?>/images/parisnoir_14.jpg" alt="Paris Photo by Adrian Unger">      
-    </div>
+  <section>
+    <figure class="contain">
+    <img src="<?php echo $base_url ?>/images/parisnoir_12.jpg" alt="Paris Photo by Adrian Unger">      
+    </figure>
+  </section>
 
-    <div>
-      <img src="<?php echo $base_url ?>/images/parisnoir_12.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
+  <section class=right>
+    <figure class="contain">
+    <img src="<?php echo $base_url ?>/images/parisnoir_3.jpg" alt="Paris Photo by Adrian Unger">
+      
+    </figure>
+  </section>
 
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/parisnoir_3.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
+  <section class=x>
+    <img src="<?php echo $base_url ?>/images/parisnoir_4.jpg" alt="Paris Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
 
-    <div class=x>
-      <img src="<?php echo $base_url ?>/images/parisnoir_4.jpg" alt="Paris Photo by Adrian Unger">
-    </div>
+<?php 
+$in_viewport = true;
+include $config['dirs']['layout'].'/footer.php'
+?>
 
-  </div>
-</div>
-
-<?php include $config['dirs']['layout'].'/footer.php' ?>
