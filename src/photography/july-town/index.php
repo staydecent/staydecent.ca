c4637a8dde6471473556fa7632f6242e3b92a76e
Adrian Unger <dev@staydecent.ca>
2024-02-25 14:15:07 -0800

    update july town.

--- src/photography/july-town/index.php
+++ src/photography/july-town/index.php
@@ -1,109 +1,125 @@
 <?php 
-  $title = 'July Town';
-  $page_id = 'town';
+$title = 'July Town';
+$page_id = 'july-town';
+$body_class = 'photography';
 ?>
 
 <?php include $config['dirs']['layout'].'/header.php' ?>
 <?php include $config['dirs']['layout'].'/nav.php' ?>
 
 <?php 
-  $base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
+$base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
 ?>
 
-<div class='aside wide'>
-  <div class=title>
-    <h1>A Small Canadian Town In July</h1>
-  </div>
+<main>
 
-  <div class=body>
-    <p class=bigger>I recently moved to a new town. Like most Canadian towns, it's full of daylight and energy during the month of July, with festivities and evenings that stretch on and on.</p>
-  </div>
-</div>
-
-<div class=container>
-  <div class="content spaced">
-
-    <div class=center>
-      <img src="<?php echo $base_url ?>/images/town_14.jpg" alt="July Town Photo by Adrian Unger">
+  <section>
+    <div class=f-center>
+      <h1 class=break>A Small Canadian Town In July</h1>
     </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/town_16.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-        <img src="<?php echo $base_url ?>/images/town_15.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-        <img src="<?php echo $base_url ?>/images/town_19.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/town_12.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
-      <img src="<?php echo $base_url ?>/images/town_7.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/town_5.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/town_6.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div>
-      <img src="<?php echo $base_url ?>/images/town_8.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/town_4.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/town_3.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/town_2.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/town_21.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/town_1.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
-      <img src="<?php echo $base_url ?>/images/town_23.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/town_18.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
-      <img src="<?php echo $base_url ?>/images/town_20.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=center>
-      <img src="<?php echo $base_url ?>/images/town_31.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=left>
-      <img src="<?php echo $base_url ?>/images/town_27.jpg" alt="July Town Photo by Adrian Unger">
-    </div>
-
-    <div class=right>
+    <div class="f-end">
       <img src="<?php echo $base_url ?>/images/town_26.jpg" alt="July Town Photo by Adrian Unger">
     </div>
+  </section>
 
-  </div>
-</div>
+  <section>
+    <figure class="contain">
+      <img src="<?php echo $base_url ?>/images/town_14.jpg" alt="July Town Photo by Adrian Unger">
+    </figure>
+  </section>
 
-<?php include $config['dirs']['layout'].'/footer.php' ?>
+  <section>
+    <div class=f-end>
+      <article>
+        <p>I recently moved to a new town. Like most Canadian towns, it's full of daylight and energy during the month of July, with festivities and evenings that stretch on&nbsp;and&nbsp;on.</p>        
+      </article>
+    </div>
+    <div class="f-end">
+      <img src="<?php echo $base_url ?>/images/town_19.jpg" alt="July Town Photo by Adrian Unger">
+    </div>
+  </section>
+
+  <section>
+    <figure class="contain">
+      <img src="<?php echo $base_url ?>/images/town_16.jpg" alt="July Town Photo by Adrian Unger">
+    </figure>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_15.jpg" alt="July Town Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_12.jpg" alt="July Town Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_7.jpg" alt="July Town Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_6.jpg" alt="July Town Photo by Adrian Unger">
+    <img src="<?php echo $base_url ?>/images/town_5.jpg" alt="July Town Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_8.jpg" alt="July Town Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
+
+
+  <section>
+    <article class=f-center>
+      <img src="<?php echo $base_url ?>/images/town_4.jpg" alt="July Town Photo by Adrian Unger">
+    </article>
+    <div class='f-end'>
+      <article class=f-center>
+        <img src="<?php echo $base_url ?>/images/town_3.jpg" alt="July Town Photo by Adrian Unger">
+      </article>
+    </div>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_2.jpg" alt="July Town Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
+
+  <section>
+    <div>&nbsp;</div>
+    <img src="<?php echo $base_url ?>/images/town_21.jpg" alt="July Town Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <figure class=contain>
+      <img src="<?php echo $base_url ?>/images/town_1.jpg" alt="July Town Photo by Adrian Unger">      
+    </figure>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_23.jpg" alt="July Town Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_20.jpg" alt="July Town Photo by Adrian Unger">
+    <img src="<?php echo $base_url ?>/images/town_18.jpg" alt="July Town Photo by Adrian Unger">
+  </section>
+
+  <section>
+    <div>&nbsp;</div>
+    <div class='f-end'>
+      <article class=f-center>
+        <img src="<?php echo $base_url ?>/images/town_31.jpg" alt="July Town Photo by Adrian Unger">      
+      </article>
+    </div>
+  </section>
+
+  <section>
+    <img src="<?php echo $base_url ?>/images/town_27.jpg" alt="July Town Photo by Adrian Unger">
+    <div>&nbsp;</div>
+  </section>
+
+<?php 
+$in_viewport = true;
+include $config['dirs']['layout'].'/footer.php'
+?>
