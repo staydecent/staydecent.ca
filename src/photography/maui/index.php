930184100b11fb10e978a6820c04826c813022d1
Adrian Unger <dev@staydecent.ca>
2024-11-03 16:48:43 -0800

    Rough in photo pages.

--- src/photography/maui/index.php
+++ src/photography/maui/index.php
@@ -11,76 +11,74 @@
   $base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
 ?>
 
-<main>
+<article id="picos">
+  <div class="article-intro">
+    <h2 class="center">Maui</h2>
+    <p class="display">10 days packed with rugged coastlines, beaches of assorted colours, roads to waterfalls, and dense tropical jungles.</p>
+  </div>
 
-  <section>
-    <div class=f-center>
-      <h1>Maui</h1>
-      <p class=display>10 days packed with rugged coastlines, beaches of assorted colours, roads to waterfalls, and dense tropical jungles.</p>
-    </div>
-    <div class=f-center>
-      <img src="<?php echo $base_url ?>/images/maui_1.jpg" alt="Maui Photo by Adrian Unger">
-    </div>
-  </section>
+  <section class="cols">
 
-  <section>
+
+  <div>
     <figure class=contain>
       <img src="<?php echo $base_url ?>/images/maui_2.jpg" alt="Maui Photo by Adrian Unger">
     </figure>
-  </section>
+  </div>
 
-  <section>
+  <div>
     <img src="<?php echo $base_url ?>/images/maui_6.jpg" alt="Maui Photo by Adrian Unger">
     <img src="<?php echo $base_url ?>/images/maui_4.jpg" alt="Maui Photo by Adrian Unger">
-  </section>
+  </div>
 
-  <section>
+  <div>
     <figure>
       <img src="<?php echo $base_url ?>/images/maui_5.jpg" alt="Maui Photo by Adrian Unger">      
     </figure>
-  </section>
+  </div>
 
-  <section>
+  <div>
     <figure class=contain>
       <img src="<?php echo $base_url ?>/images/maui_3.jpg" alt="Maui Photo by Adrian Unger">      
     </figure>
-  </section>
+  </div>
 
-  <section>
+  <div>
     <div>
       <img src="<?php echo $base_url ?>/images/maui_7.jpg" alt="Maui Photo by Adrian Unger">
     </div>
     <div class=f-end>
       <img src="<?php echo $base_url ?>/images/maui_8.jpg" alt="Maui Photo by Adrian Unger">
     </div>
-  </section>
+  </div>
 
-  <section>
+  <div>
     <figure class="contain">
       <img src="<?php echo $base_url ?>/images/maui_9.jpg" alt="Maui Photo by Adrian Unger">      
     </figure>
     <figure class="contain">
       <img src="<?php echo $base_url ?>/images/maui_10.jpg" alt="Maui Photo by Adrian Unger">      
     </figure>
-  </section>
+  </div>
 
-  <section>
+  <div>
     <figure class="cover">
       <img src="<?php echo $base_url ?>/images/maui_12.jpg" alt="Maui Photo by Adrian Unger">      
     </figure>
     <figure class="cover">
       <img src="<?php echo $base_url ?>/images/maui_11.jpg" alt="Maui Photo by Adrian Unger">      
     </figure>
-  </section>
+  </div>
 
-  <section>
+  <div>
     <figure class=contain>
       <img src="<?php echo $base_url ?>/images/maui_13.jpg" alt="Maui Photo by Adrian Unger">
     </figure>
-  </section>
+  </div>
+
+</section>
 
 <?php 
-$in_viewport = true;
 include $config['dirs']['layout'].'/footer.php'
 ?>
 
--- src/photography/waves/index.php
+++ src/photography/waves/index.php
@@ -11,78 +11,72 @@
   $base_url = SITE_URL . str_replace(SOURCE, '', __DIR__);
 ?>
 
+<article id="picos">
+  <div class="article-intro">
+    <h2 class="center">Waves</h2>
+    <p class="display">A cold, but sunny day during winter, we headed to the west coast.</p>
+  </div>
 
-<main>
-
-  <section>
-    <div class=f-center>
-      <h1>Waves</h1>
-      <p class=display>A cold, but sunny day during winter, we headed to the west coast.</p>
-    </div>
-    <div class=f-center>
-      <img src="<?php echo $base_url ?>/images/waves_4.jpg" alt="Maui Photo by Adrian Unger">
-    </div>
-  </section>
-
-  <section>
-    <img src="<?php echo $base_url ?>/images/waves_2.jpg" alt="Waves Photo by Adrian Unger">
-  </section>
-
-  <section>
-    <div class=no-mobile>&nbsp;</div>
-    <img src="<?php echo $base_url ?>/images/waves_6.jpg" alt="Waves Photo by Adrian Unger">
-  </section>
-
-  <section>
-    <img src="<?php echo $base_url ?>/images/waves_7.jpg" alt="Waves Photo by Adrian Unger">
-    <div class=no-mobile>&nbsp;</div>
-  </section>
-
-  <section class=center>
-    <img src="<?php echo $base_url ?>/images/waves_8.jpg" alt="Waves Photo by Adrian Unger">
-    <div class=no-mobile>&nbsp;</div>
-  </section>
-
-  <section>
-    <figure class="contain">
-    <img src="<?php echo $base_url ?>/images/waves_3.jpg" alt="Waves Photo by Adrian Unger">
-    </figure>
-  </section>
-
-  <section>
-    <div class=no-mobile>&nbsp;</div>
-    <img src="<?php echo $base_url ?>/images/waves_5.jpg" alt="Waves Photo by Adrian Unger">
-  </section>
-
-  <section>
-    <figure class="contain">
-    <img src="<?php echo $base_url ?>/images/waves_12.jpg" alt="Waves Photo by Adrian Unger">
-    </figure>
-  </section>
-
-
-  <section>
-    <div class=f-end>
-      <img src="<?php echo $base_url ?>/images/waves_9.jpg" alt="Waves Photo by Adrian Unger">
-    </div>
+  <section class="cols">
     <div>
-    <img src="<?php echo $base_url ?>/images/waves_11.jpg" alt="Waves Photo by Adrian Unger">
+      <img src="<?php echo $base_url ?>/images/waves_2.jpg" alt="Waves Photo by Adrian Unger">
     </div>
-  </section>
 
-  <section>
-    <div class=no-mobile>&nbsp;</div>
-    <img src="<?php echo $base_url ?>/images/waves_13.jpg" alt="Waves Photo by Adrian Unger">      
-  </section>
+    <div>
+      <div class=no-mobile>&nbsp;</div>
+      <img src="<?php echo $base_url ?>/images/waves_6.jpg" alt="Waves Photo by Adrian Unger">
+    </div>
 
-  <section>
-    <img src="<?php echo $base_url ?>/images/waves_10.jpg" alt="Waves Photo by Adrian Unger">
-    <div class=no-mobile>&nbsp;</div>
-      
+    <div>
+      <img src="<?php echo $base_url ?>/images/waves_7.jpg" alt="Waves Photo by Adrian Unger">
+      <div class=no-mobile>&nbsp;</div>
+    </div>
+
+    <div class=center>
+      <img src="<?php echo $base_url ?>/images/waves_8.jpg" alt="Waves Photo by Adrian Unger">
+      <div class=no-mobile>&nbsp;</div>
+    </div>
+
+    <div>
+      <figure class="contain">
+      <img src="<?php echo $base_url ?>/images/waves_3.jpg" alt="Waves Photo by Adrian Unger">
+      </figure>
+    </div>
+
+    <div>
+      <div class=no-mobile>&nbsp;</div>
+      <img src="<?php echo $base_url ?>/images/waves_5.jpg" alt="Waves Photo by Adrian Unger">
+    </div>
+
+    <div>
+      <figure class="contain">
+      <img src="<?php echo $base_url ?>/images/waves_12.jpg" alt="Waves Photo by Adrian Unger">
+      </figure>
+    </div>
+
+
+    <div>
+      <div class=f-end>
+        <img src="<?php echo $base_url ?>/images/waves_9.jpg" alt="Waves Photo by Adrian Unger">
+      </div>
+      <div>
+      <img src="<?php echo $base_url ?>/images/waves_11.jpg" alt="Waves Photo by Adrian Unger">
+      </div>
+    </div>
+
+    <div>
+      <div class=no-mobile>&nbsp;</div>
+      <img src="<?php echo $base_url ?>/images/waves_13.jpg" alt="Waves Photo by Adrian Unger">      
+    </div>
+
+    <div>
+      <img src="<?php echo $base_url ?>/images/waves_10.jpg" alt="Waves Photo by Adrian Unger">
+      <div class=no-mobile>&nbsp;</div>
+        
+    </div>
   </section>
 
 <?php 
-$in_viewport = true;
 include $config['dirs']['layout'].'/footer.php'
 ?>
 
