b096dfe096f80a7b3310c119b2b569dc10ed4a4b
Adrian Unger <dev@staydecent.ca>
2025-10-07 19:16:08 -0700

    Update homepage to link to projects.

--- src/pages/heybear.php
+++ src/pages/heybear.php
@@ -12,21 +12,6 @@ $title = 'Hey Bear! Newsletter written by Adrian Unger';
 
   <section class=cols>
     <div>
-      <p><strong>Where I hope to convey the joy, delight and awe that I experience from nature.</strong> It’s remarkable we have a liveable planet at all—ya know, considering how <em>unlivable</em> the rest of the known universe is—let alone a planet with such abundance and majesty! I know of no greater glee than when I am privy to animals simply living their lives. </p>
-
-      <p>It’s also a bit of a selfish project as I enjoy writing and taking photos and this will give me somewhere to house those, rather than letting them rot in some dark corner of Apple's cloud storage.</p>
-
-      <p>I also—a bit presumptuously I might add—feel that, more than ever, we humans could benefit from greater connection to this planet which is an inexorable part of our existence. Even, simply, to bring more attention to the cyclical nature of—<em>ahem</em>—Nature. </p>
-
-      <p>Uncertainty is part of reality just as much as this Earth is. And, we seem to be living in especially uncertain times. Yet, the rhythm of the seasons remains somewhat constant. The sun rises every morning and the moon every night. Opinions and politics don’t change that. Revelling in these rhythms grounds me into actual reality. </p>
-
-      <p>So that’s my goal: to share my delight from the natural world and to revel in the unavoidable rhythms of our reality. It should be fun!</p>
-
-      <p>👋🐻</p>
-
-      <p><small><em>Curious about the name? Read the first issue ;) </em></small></p>
-    </div>
-    <div class=mobile-first>
       <div class="block">
         <h3>Subscribe to Hey Bear!</h3>
         <p>Revelations from Nature. Just honest-to-goodness nuggets of joy and delight with the occasional self-reflection.</p>
@@ -43,6 +28,23 @@ $title = 'Hey Bear! Newsletter written by Adrian Unger';
           <p><small>I'll never spam or share your email with anyone.</small></p>
         </form>
       </div>
+
+      <p><strong>Where I hope to convey the joy, delight and awe that I experience from nature.</strong> It’s remarkable we have a liveable planet at all—ya know, considering how <em>unlivable</em> the rest of the known universe is—let alone a planet with such abundance and majesty! I know of no greater glee than when I am privy to animals simply living their lives. </p>
+
+      <p>It’s also a bit of a selfish project as I enjoy writing and taking photos and this will give me somewhere to house those, rather than letting them rot in some dark corner of Apple's cloud storage.</p>
+
+      <p>I also—a bit presumptuously I might add—feel that, more than ever, we humans could benefit from greater connection to this planet which is an inexorable part of our existence. Even, simply, to bring more attention to the cyclical nature of—<em>ahem</em>—Nature. </p>
+
+      <p>Uncertainty is part of reality just as much as this Earth is. And, we seem to be living in especially uncertain times. Yet, the rhythm of the seasons remains somewhat constant. The sun rises every morning and the moon every night. Opinions and politics don’t change that. Revelling in these rhythms grounds me into actual reality. </p>
+
+      <p>So that’s my goal: to share my delight from the natural world and to revel in the unavoidable rhythms of our reality. It should be fun!</p>
+
+      <p>👋🐻</p>
+
+      <p><small><em>Curious about the name? Read the first issue ;) </em></small></p>
+    </div>
+    <div class=mobile-first>
+      <img class="contain" src="<?php echo SITE_URL ?>assets/media/heybear.jpg">
     </div>
   </section>  
 </article>
--- src/pages/index.php
+++ src/pages/index.php
@@ -7,62 +7,30 @@ $body_class = 'text';
 <article>
   <section class="cols">
     <div>
-      <img class="contain" src="<?php echo SITE_URL ?>photography/sebastion-beach/images/sebastion_10.jpg">
+      <img class="contain" src="<?php echo SITE_URL ?>assets/media/heybear.jpg">
       <div class=title>
-        <a class=plain href="<?php echo SITE_URL ?>photography/sebastion-beach">Sebastion Beach →</a>
+        <a class=plain href="<?php echo SITE_URL ?>heybear">Hey Bear! →</a>
+      </div>
+    </div>
+
+    <div>
+      <img class="contain" src="<?php echo SITE_URL ?>assets/media/tednet.jpg">
+      <div class=title>
+        <a class=plain href="https://tednet.org" target="_blank">The Existential Detective Network <span class="mono">↗</span></a>
       </div>
     </div>
 
     <div>
       <img class="contain" src="<?php echo SITE_URL ?>photography/july-town/images/town_14.jpg">
       <div class=title>
-        <a class=plain href="<?php echo SITE_URL ?>photography/july-town">A Small Canadian Town in July →</a>
+        <a class=plain href="<?php echo SITE_URL ?>photography">Photography →</a>
       </div>
     </div>
 
     <div>
-      <img class="contain" src="<?php echo SITE_URL ?>photography/picos-on-portra/images/picos_11.jpg">
+      <img class="contain" src="<?php echo SITE_URL ?>assets/media/pathofheart-flowers.jpg">
       <div class=title>
-        <a class=plain href="<?php echo SITE_URL ?>photography/picos-on-portra">Picos on Portra →</a>
-      </div>
-    </div>
-
-    <div class=right>
-      <div>
-        <img class="contain" src="<?php echo SITE_URL ?>photography/paris-noir/images/parisnoir_7.jpg">
-        <div class=title>
-          <a class=plain href="<?php echo SITE_URL ?>photography/paris-noir">Paris Noir →</a>
-        </div>
-      </div>
-    </div>
-
-    <div>
-      <img class="contain" src="<?php echo SITE_URL ?>photography/paris-couleur/images/pariscouleur_8.jpg">
-      <div class=title>
-        <a class=plain href="<?php echo SITE_URL ?>photography/paris-couleur">Paris Coleur →</a>
-      </div>
-    </div>
-
-    <div>
-      <img class="contain" src="<?php echo SITE_URL ?>photography/waves/images/waves_2.jpg">
-      <div class=title>
-        <a class=plain href="<?php echo SITE_URL ?>photography/waves">Waves →</a>
-      </div>
-    </div>
-
-    <div>
-      <img class="contain" src="<?php echo SITE_URL ?>photography/maui/images/maui_5.jpg">
-      <div class=title>
-        <a class=plain href="<?php echo SITE_URL ?>photography/maui">Maui →</a>
-      </div>
-    </div>
-
-    <div class=right>
-      <div>
-        <img class="contain" src="<?php echo SITE_URL ?>photography/black-and-white/images/bnw_10.jpg">
-        <div class=title>
-          <a class=plain href="<?php echo SITE_URL ?>photography/black-and-white">Black &amp; White →</a>
-        </div>
+        <a class=plain href="https://pathofheart.ca" target="_blank">Path of Heart <span class="mono">↗</span></a>
       </div>
     </div>
 
