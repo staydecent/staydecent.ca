e276624ea596729e4294434e28a53a2f2bad4410
Adrian Unger <dev@staydecent.ca>
2025-12-19 12:05:02 -0800

    touch up pages, articles, etc.

--- src/pages/sabbatical.php
+++ src/pages/sabbatical.php
@@ -9,6 +9,7 @@
     <p class=bigger>Having the luxury to take a sabbatical is a wonderful thing.</p>
     <p>For me, I reached a point where it was clear I wasn't really following my heart when it came to my work. I actually have had the luck of having a wonderful career working with even more wonderful people. Despite this, I still would reach a place where I felt stuck, unfulfilled, and would create my own tension and stories to explain why the job was not for me anymore. Only with my most recent job did I recognize that it was all my own creation. It forced me to really look at my conditioned and unconscious behavior. Thus, the sabbatical.</p>
     <p>I can't say anything truly new or profound has come of my time to think and process. Really, it seems most if not all of life's problems have been answered in some way by philosophers and spiritual teachers of the past. In any case, there are a few quips and viewpoints that exemplify the experience I've had. I aim to collect them here and update this as my views change and experience continues to unfold.</p>
+    <p>As I've been making sense of my situation I've found that <a href="<?php echo SITE_URL ?>writing">writing</a> has been a necessity. It seems I don't actually know what I'm thinking unless I write.</p>
   </div>
 
   <section class="cols">
---
+++ src/pages/writing.php
@@ -0,0 +1,20 @@
+<?php
+$page_id = 'writing';
+$body_class = 'text';
+?>
+<?php include $config['dirs']['layout'].'/header.php' ?>
+
+<article>
+  <div class="article-intro">
+    <h2>Writing.</h2>
+  </div>
+  <section class=cols>
+    <div>
+      <p>The pandemic kicked off what would be the <a href="https://tednet.org/newsletter/how-i-became-an-ascended-master/" target="_blank" title="How I became an ascended master">shattering of my worldview</a>&mdash;one based purely on logic and reason and rationality and the delusion that that was enough to understand the world. The only way for me to even realise this and become conscious of it was to start writing, without for which I was nearly as unaware of my own internal state as that of another.</p>
+      <p>Journaling is good. Sometimes I just need to get some emotion out through that process. Yet, as I was still wrestling with losing the security of my worldview, I found countless worldviews of others that offered a sort of temporary refrain. It was easy to fall into the comfort of someone else's creation, of someone else's story.</p>
+      <p>By writing out this process <a href="https://tednet.org" target="_blank" title="The Existential Detective Network">publically</a>, it forced me to be more discerning. It's easy to write for one's self, in private, and have no stake and thus fall into any whim or fantasy.</p>
+      <p>I also take great pleasure in reading other people's personal blogs and explorations. Hopefully, I can add to that.</p>
+  </section>  
+</article>
+
+<?php include $config['dirs']['layout'].'/footer.php' ?>
